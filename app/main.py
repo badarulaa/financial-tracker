9211b4baa1bdaa14659078cbeca9e944048927ab
badarulaa <badarul93@gmail.com>
2026-03-05 15:17:42 +0700

    add main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,16 @@
 from app.database import engine
 from app.models import Base
+from fastapi import FastAPI
+from app.scheduler import start_scheduler
+
+app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
+
+@app.on_event("startup")
+def startup_event():
+  start_scheduler()
+
+@app.get("/")
+def root():
+  return {"status": "Financial Tracker is Running"}
